4b11e8aaa7d632e2a531341f5eae0c61544a72ad
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-09 14:15:32 +0300

    Implement expanding of the clicked image using event delegation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const container = document.querySelector('.container');
+const panels = document.querySelectorAll('.panel');
+
+container.addEventListener('click', function (event) {
+  if (event.target.classList.contains('panel')) {
+    const panel = event.target;
+    const siblings = panel.closest('.container').querySelectorAll('.panel');
+    siblings.forEach(sibling => {
+      if (sibling !== panel) sibling.classList.remove('panel--active');
+    });
+    panel.classList.add('panel--active');
+  }
+});
