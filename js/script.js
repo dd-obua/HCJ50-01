f3614a06d1ada0ec1aeb9befd38151e00be352a1
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-09 14:40:33 +0300

    Prevent event default behaviour

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ const activatePanel = panel => panel.classList.add('panel--active');
 const deactivatePanel = panel => panel.classList.remove('panel--active');
 
 container.addEventListener('click', function (event) {
+  event.preventDefault();
+
   if (event.target.classList.contains('panel')) {
     const panel = event.target;
     const siblings = panel.closest('.container').querySelectorAll('.panel');
